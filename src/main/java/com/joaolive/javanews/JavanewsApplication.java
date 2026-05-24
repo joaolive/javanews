5371615e9cee90ac54a0216f24acf860aaa9be03
joaolive <devjoaolive@outlook.com>
2026-05-23 21:55:57 -0300

    chore: init project

---
+++ src/main/java/com/joaolive/javanews/JavanewsApplication.java
@@ -0,0 +1,13 @@
+package com.joaolive.javanews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavanewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavanewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaolive/javanews/JavanewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaolive.javanews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavanewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
