01dcfc00d0ba811622289230d684d080eded6fd1
joaolive <devjoaolive@outlook.com>
2026-05-24 17:08:01 -0300

    refactor(user): convert Name record to class with factory methods

--- src/main/java/com/joaolive/javanews/user/domain/valueobject/Name.java
+++ src/main/java/com/joaolive/javanews/user/domain/valueobject/Name.java
@@ -2,10 +2,20 @@ package com.joaolive.javanews.user.domain.valueobject;
 
 import java.util.Objects;
 
-public record Name(String value) {
-	public Name {
+public class Name {
+	private final String value;
+
+	private Name(String value) {
+		this.value = value;
+	}
+	public static Name create(String value) {
 		Objects.requireNonNull(value, "Name cannot be null");
 		if (value.isBlank())
 			throw new IllegalArgumentException("Name cannot be null");
+		return new Name(value);
+	}
+
+	public static Name restore(String value) {
+		return new Name(value);
 	}
 }
