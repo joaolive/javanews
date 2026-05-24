0e3f8b10781b0b9d227cbf8b84e067f79ab073bf
joaolive <devjoaolive@outlook.com>
2026-05-24 18:16:44 -0300

    feat(infrastructure): implement UserRepositoryAdapter with JPA and mapper

---
+++ src/main/java/com/joaolive/javanews/user/infrastructure/adapter/out/persistence/UserJpaRepository.java
@@ -0,0 +1,9 @@
+package com.joaolive.javanews.user.infrastructure.adapter.out.persistence;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserJpaRepository extends JpaRepository<UserEntity, UUID> {
+	boolean existsByUsername(String username);
+}
