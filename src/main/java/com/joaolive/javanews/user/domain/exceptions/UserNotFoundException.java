abe00111ae7bdbef99b694edf06495910b1c3c35
joaolive <devjoaolive@outlook.com>
2026-05-24 21:16:05 -0300

    refactor(dev): remove unused files

--- src/main/java/com/joaolive/javanews/user/domain/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.joaolive.javanews.user.domain.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-	public UserNotFoundException(String msg) {
-		super(msg);
-	}
-}
