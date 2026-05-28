6704e212e594d41a5b319783e274b1739fccc460
joaolive <devjoaolive@outlook.com>
2026-05-28 16:07:50 -0300

    feat(post): add TagEntity persistence model

---
+++ src/main/java/com/joaolive/javanews/post/infrastructure/adapter/out/persistence/TagEntity.java
@@ -0,0 +1,26 @@
+package com.joaolive.javanews.post.infrastructure.adapter.out.persistence;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "tb_tag")
+public class TagEntity {
+	@Id
+	private UUID	id;
+	@Column(unique = true, length = 32, nullable = false)
+	private String	name;
+}
