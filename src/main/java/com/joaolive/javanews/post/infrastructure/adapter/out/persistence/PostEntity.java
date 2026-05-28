975d1f94c0b0a6aecbe2fe7e935885e306a4594a
joaolive <devjoaolive@outlook.com>
2026-05-28 16:45:13 -0300

    feat(post): add PostEntity persistence model

---
+++ src/main/java/com/joaolive/javanews/post/infrastructure/adapter/out/persistence/PostEntity.java
@@ -0,0 +1,69 @@
+package com.joaolive.javanews.post.infrastructure.adapter.out.persistence;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "tb_post")
+public class PostEntity {
+	@Id
+	private UUID	id;
+	private String	title;
+	private String	slug;
+	@Column(columnDefinition = "TEXT")
+	private String	body;
+	@Column(name = "author_id")
+	private UUID	authorId;
+	private Instant	createdAt;
+	private Instant	updatedAt;
+
+	@Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    @ManyToMany
+    @JoinTable(
+		name = "tb_post_tag",
+		joinColumns = @JoinColumn(name = "post_id"),
+		inverseJoinColumns = @JoinColumn(name = "tag_id"))
+	private Set<TagEntity>	tags = new HashSet<>();
+
+	public PostEntity(UUID id, String title, String slug, String body, UUID authorId, Instant createdAt,
+			Instant updatedAt) {
+		this.id = id;
+		this.title = title;
+		this.slug = slug;
+		this.body = body;
+		this.authorId = authorId;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+
+	public Set<TagEntity> getTags() {
+		return Collections.unmodifiableSet(this.tags);
+	}
+
+	public void addTag(TagEntity tag) {
+		this.tags.add(tag);
+	}
+
+	public void removeTag(TagEntity tag) {
+		this.tags.remove(tag);
+	}
+}
