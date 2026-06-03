6cd546f3711ccb946c195254bd936c5055a847d3
joaolive <devjoaolive@outlook.com>
2026-06-03 14:31:59 -0300

    refactor(auth): remove obsolete H2 security configuration

--- src/main/java/com/joaolive/javanews/auth/config/ResourceServerConfig.java
+++ src/main/java/com/joaolive/javanews/auth/config/ResourceServerConfig.java
@@ -31,16 +31,6 @@ public class ResourceServerConfig {
 	private String corsOrigins;
 
 	@Bean
-	@Profile("test")
-	@Order(1)
-	SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception {
-
-		http.securityMatcher(PathRequest.toH2Console()).csrf(csrf -> csrf.disable())
-				.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
-		return http.build();
-	}
-
-	@Bean
 	@Order(3)
 	SecurityFilterChain rsSecurityFilterChain(HttpSecurity http) throws Exception {
 
