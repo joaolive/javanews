c31fd1cea3945c9409d6ff8ba0e23fe6aea3e993
joaolive <devjoaolive@outlook.com>
2026-06-02 23:22:05 -0300

    refactor(modulith): rename shared module reference to core

--- src/main/java/com/joaolive/javanews/JavanewsApplication.java
+++ src/main/java/com/joaolive/javanews/JavanewsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.modulith.Modulithic;
 
 @SpringBootApplication
-@Modulithic(sharedModules = "common")
+@Modulithic(sharedModules = "core")
 public class JavanewsApplication {
 
 	public static void main(String[] args) {
