ab8accf8132032d58d14d7493bcd35163aa8650e
joaolive <devjoaolive@outlook.com>
2026-06-05 15:10:16 -0300

    feat(post): update post response model for comment support

--- src/main/java/com/joaolive/javanews/post/infrastructure/web/response/PostResponse.java
+++ src/main/java/com/joaolive/javanews/post/infrastructure/web/response/PostResponse.java
@@ -5,10 +5,16 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.joaolive.javanews.post.domain.Post;
+import com.joaolive.javanews.post.domain.valueobject.Slug;
+import com.joaolive.javanews.post.domain.valueobject.Title;
 
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record PostResponse(
 	UUID id,
+	UUID parentId,
+	String type,
 	String title,
 	String slug,
 	String body,
@@ -20,8 +26,10 @@ public record PostResponse(
 	public static PostResponse from(Post domain) {
 		return new PostResponse(
 			domain.getId(),
-			domain.getTitle().value(),
-			domain.getSlug().value(),
+			domain.getParentId(),
+			domain.getType().name(),
+			domain.getTitle().map(Title::value).orElse(null),
+			domain.getSlug().map(Slug::value).orElse(null),
 			domain.getBody().value(),
 			domain.getAuthorId(),
 			domain.getCreatedAt(),
